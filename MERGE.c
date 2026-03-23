30598b12013a4e8b83dee35747ee2ac86bcabcf0
Ashwath9191 <ashwath.srinivas91@gmail.com>
2026-03-23 22:10:37 +0530

    Add files via upload

---
+++ MERGE.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define N 12
+
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int roll[N] = {157, 110, 147, 122, 111, 149, 151, 141, 123, 112, 117, 133};
+
+    mergeSort(roll, 0, N - 1);
+
+    printf("Sorted Roll Numbers using Merge Sort:\n");
+    for (int i = 0; i < N; i++)
+        printf("%d ", roll[i]);
+
+    return 0;
+}
